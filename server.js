e47cbd3004725d8d63b12bc319f546848b8abf65
Nirajan Mahara <mnirajan9@gmail.com>
2021-11-01 16:08:37 +0545

    Cleanup - `posts` routes

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ app.use(express.json());
 app.set("view engine", "ejs");
 
 const userRouter = require("./routes/users");
-const postRouter = require("./routes/posts");
 
 app.use("/users", userRouter);
-app.use("/posts", postRouter);
 
 app.listen(3000);
