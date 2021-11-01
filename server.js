c8cd2028206d7647031537da5d5b1ae6068a3ab2
Nirajan Mahara <mnirajan9@gmail.com>
2021-11-01 12:13:54 +0545

    `res.status(500).json({ message: "Error" });` - send status and json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   console.log("Here in console");
-  res.status(500).send("Hello World");
+  res.status(500).json({ message: "Error" });
 });
 
 app.listen(3000);
