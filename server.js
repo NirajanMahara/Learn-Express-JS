8c1ea1cc6bd2821c4f11103aea549f892d0ea88f
Nirajan Mahara <mnirajan9@gmail.com>
2021-11-01 12:30:46 +0545

    Pass `text` variable from `server` to `client ejs`

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   console.log("Here in console");
-  res.render("index");
+  res.render("index", { text: "Nirajan" });
 });
 
 app.listen(3000);
