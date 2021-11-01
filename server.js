4b39512faaeb185723a0dd791fa2a2dc2d20abda
Nirajan Mahara <mnirajan9@gmail.com>
2021-11-01 12:33:45 +0545

    Using `locals` to Pass `text` variable from `server` to `client ejs` || default

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   console.log("Here in console");
-  res.render("index", { text: "Nirajan" });
+  res.render("index", { name: "Nirajan" });
 });
 
 app.listen(3000);
